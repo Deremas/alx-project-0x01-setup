576f311e787d1707ad26d76a201aaf14c529c23f
Dereje Masresha <derejemasresha27@gmail.com>
2025-09-18 13:48:08 +0300

    modal done for Add post

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,8 +1,17 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostProps, PostData } from "@/interfaces";
+import { useState } from "react";
 
 const Posts: React.FC<PostProps[]> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
+
+  const handdleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
+
   console.log(posts);
   return (
     <div className="flex flex-col h-screen">
@@ -10,7 +19,10 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
       <main className="p-4">
         <div className="flex justify-between">
           <h1 className=" text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
             Add Post
           </button>
         </div>
@@ -26,6 +38,12 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
           ))}
         </div>
       </main>
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handdleAddPost}
+        />
+      )}
     </div>
   );
 };
